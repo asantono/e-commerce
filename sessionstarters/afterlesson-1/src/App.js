923913e8238d5f8e0985551bad0b9462e7698003
asantono <41930466+asantono@users.noreply.github.com>
2020-09-05 16:42:49 -0400

    updating afterlesson-1 to mirror all video changes

---
+++ sessionstarters/afterlesson-1/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CtaTop from "./components/ctaTop/CtaTop";
+import "./styles/reset.css";
+import "./styles/cta-top.css";
+
+function App() {
+  return <CtaTop />;
+}
+
+export default App;
